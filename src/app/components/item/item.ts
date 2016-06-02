e590a51e71a34d2060b7c804d1b4d2cf89704646
Kevin Cando <kevincando@Sucres-iMac.local>
2016-06-02 07:32:48 -0500

    Se realizaron cambios para poder tener una funcionalidad mas modal

---
+++ src/app/components/item/item.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by kevincando on 6/2/16.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'item',
+  templateUrl: 'app/components/item/item.html',
+  styleUrls: ['app/components/item/item.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class Item {
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
+}
--- src/app/seed-app.ts
+++ src/app/seed-app.ts
@@ -4,6 +4,7 @@ import {Router, RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated
 import {Home} from './components/home/home';
 import {About} from './components/about/about';
 import {RepoBrowser} from './components/repo-browser/repo-browser';
+import {Item} from './components/item/item'
 
 @Component({
   selector: 'seed-app',
@@ -16,6 +17,7 @@ import {RepoBrowser} from './components/repo-browser/repo-browser';
   { path: '/home',       component: Home,        name: 'Home', useAsDefault: true },
   { path: '/about',      component: About,       name: 'About' },
   { path: '/github/...', component: RepoBrowser, name: 'RepoBrowser' },
+  { path: '/item', component: Item, name: 'Item'}
 ])
 export class SeedApp {
 
